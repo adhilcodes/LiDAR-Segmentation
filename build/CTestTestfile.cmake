88223e5a353373aaa78dfa5ac11f691ec16ce2ae
Abdul Adhil PK <65992809+adhilcodes@users.noreply.github.com>
2023-08-31 10:40:32 +0530

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adhil/Desktop/pcd_ws/src
-# Build directory: /home/adhil/Desktop/pcd_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("fcw_segment")
--- build/fcw_segment/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adhil/Desktop/pcd_ws/src/fcw_segment
-# Build directory: /home/adhil/Desktop/pcd_ws/build/fcw_segment
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
